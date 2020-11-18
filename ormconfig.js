77420f43edb6d565284dd958303c2c4b2b5a99c8
Ricardo Note <shyoutarou@gmail.com>
2020-11-18 16:25:04 -0300

    Update ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -1,20 +1,13 @@
 module.exports =  {
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "docker",
-    database: "happy",
-    migrations: [
-      "./src/database/migrations/*.ts"
-    ],
-    entities: [
-      "./src/models/*.ts"
-    ],
-    cli: {
-      "migrationsDir": "./src/database/migrations"
-    },  
-    seeds: ["./src/database/seeds/**/*{.ts,.js}"],
-    factories: ["./src/database/factories/**/*{.ts,.js}"]    
-
+  type: 'postgres',
+  host: process.env.TYPEORM_HOST,
+  port: process.env.TYPEORM_PORT,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+  entities: [process.env.TYPEORM_ENTITIES],
+  migrations: [process.env.TYPEORM_MIGRATIONS],
+  cli: {
+    migrationsDir: process.env.TYPEORM_MIGRATIONS_DIR,
+  },
 }
